5c0bc9d267fd031135022506d15179c8b4e45aac
Jorge Tapia <jorge@profapps.co>
2016-03-24 15:35:54 -0400

    Add initial project files. Add app icon.

---
+++ Footprints/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Footprints
+//
+//  Created by Jorge Tapia on 3/24/16.
+//  Copyright © 2016 Jorge Tapia. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
