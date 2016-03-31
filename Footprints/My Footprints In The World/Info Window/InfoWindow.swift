a650701e96ebe41488d1c5b360f1f84d86ec6964
Jorge Tapia <jorge@profapps.co>
2016-03-30 21:52:48 -0400

    Add My Footprints In The World.

---
+++ Footprints/My Footprints In The World/Info Window/InfoWindow.swift
@@ -0,0 +1,21 @@
+//
+//  InfoWindow.swift
+//  Footprints
+//
+//  Created by Jorge Tapia on 3/30/16.
+//  Copyright © 2016 Jorge Tapia. All rights reserved.
+//
+
+import UIKit
+
+class InfoWindow: UIView {
+    
+    @IBOutlet weak var pictureImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
--- Podfile
+++ Podfile
@@ -1,7 +1,6 @@
+platform :ios, '9.3'
 use_frameworks!
 
 target 'Footprints' do
-	source 'https://github.com/CocoaPods/Specs.git'
-	platform :ios, '8.1'
 	pod 'GoogleMaps'
 end
